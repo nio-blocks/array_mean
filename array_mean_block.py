a015a4f229f69f405221445ddbb32d9fefc30bf3
Tom Young <shadetreemetalworks@gmail.com>
2019-09-11 19:32:12 -0600

    initial commit

--- array_mean_block.py
+++ array_mean_block.py
@@ -1,12 +1,27 @@
+import numpy as np
 from nio import Block
-from nio.properties import VersionProperty
+from nio.block.mixins import EnrichSignals
+from nio.properties import Property, StringProperty, VersionProperty
 
 
-class ArrayMean(Block):
+class ArrayMean(EnrichSignals, Block):
 
+    incoming_array = Property(
+        title='Incoming Array',
+        order=0)
+    outgoing_attribute = StringProperty(
+        title='Outgoing Signal Attribute',
+        default='mean',
+        order=1)
     version = VersionProperty('0.1.0')
 
-    def process_signals(self, signals):
-        for signal in signals:
-            pass
-        self.notify_signals(signals)
+    def process_signal(self, signal):
+        array = self.incoming_array(signal)
+        if not isinstance(array, np.ndarray):
+            array = np.array(array)
+        dimensions = len(array.shape)
+        self.logger.debug('Reducing {}D matrix'.format(dimensions))
+        for r in range(dimensions - 1) or range(1):
+            array = array.mean(axis=0)
+        attr = self.outgoing_attribute(signal)
+        return self.get_output_signal({attr: array}, signal)
